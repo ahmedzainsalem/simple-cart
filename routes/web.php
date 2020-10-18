2a8e19fdabcd74180e3aa2b42846e457aa45274c
ahmedzainsalem <ahmedzainsalem@gmail.com>
2020-10-18 12:50:58 +0200

    Front - Layout and displaying products

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [
+    'uses' => 'FrontEndController@index',
+    'as' => 'index'
+]);
 
 Route::resource('products', 'ProductsController');
 Route::resource('categories', 'CategoriesController');
